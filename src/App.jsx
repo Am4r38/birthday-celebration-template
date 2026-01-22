9f95c7adb54d2996b255c6c817f44aebd36b4642
Am4r38 <pa9643854571@gmail.com>
2026-01-23 01:26:30 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -96,7 +96,7 @@ function App() {
           <h1 id="heroTitle">
             {birthdayReached ? (
               <>
-                Happy Birthday <span className="highlight">[Kiddo]</span> 🎂
+                Happy Birthday <span className="highlight">Kiddo</span> 🎂
               </>
             ) : (
               <>
